94ccd9ff6bb2bfc3edd701fab14a5015906b3a7e
Jose Jimenez <luis.jimenez@weirdneutrino.io>
2023-09-11 00:26:37 -0600

    financial Resume open details toggle

--- app/components/FinantialResume.js
+++ app/components/FinantialResume.js
@@ -1,50 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function FinantialResume(props){
     let resume_data = props.data
     let open = true
 
-    const action = () => {
-      open = !open
-    } ;
+    
+    const Row = ({item}: ItemProps) => {
+      const [isShowingDetails, setIsShowingDetails] = useState(false);
+      const action = () => {
+        setIsShowingDetails(!isShowingDetails)
+      } ;
+
+      return (
+        <View style={styles.item}>
+          <View style={{flexDirection:'row', width:'80%'}} >
+            <TouchableOpacity style={styles.ActionBtn} onPress={action} >
+              { isShowingDetails ? (
+                <Text style={styles.ButtonText}> ▲ </Text> 
+              ):(
+                <Text style={styles.ButtonText}> ▼ </Text> 
+              )}
+            </TouchableOpacity>
+            <View style={{width:'100%'}} >
+              <TouchableOpacity onPress={action} >
+                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                  <Text style={styles.title}>{item.name}</Text>
+                  <Text style={[styles.text, item.total_status == 'no-debt'? {color:'green'} : {color:'red'} ]}>{item.total_label} {item.total}</Text>
+                </View>
+              </TouchableOpacity>
+              { isShowingDetails ? (
+              <FlatList 
+                data={item.fields}
+                renderItem={({item}) => (
+                  <View style={{flexDirection:'row', justifyContent:'space-between', margin:3}}>
+                    <Text style={styles.title} >{item.label} </Text>
+                    <Text style={[styles.text, item.status == 'paid'? {color:'green'} : {color:'red'} ]}>{item.value}</Text>
+                  </View>
+                )}
+              />
+              ):(
+                null
+              )}
+            </View>
+          </View>
+        </View>
+      )
+    } 
 
     return (
       <View style ={styles.container}>
         <FlatList
             data={resume_data.rows}
-            renderItem={({ item }) => (
-              <View style={styles.item}>
-                <View style={{flexDirection:'row', width:'80%'}} >
-                  <TouchableOpacity style={styles.ActionBtn} onPress={action} >
-                    { open ? (
-                      <Text style={styles.ButtonText}> ▲ </Text> 
-                    ):(
-                      <Text style={styles.ButtonText}> ▼ </Text> 
-                    )}
-                  </TouchableOpacity>
-                  <View style={{width:'100%'}} >
-                    <View style={{flexDirection:'row', justifyContent:'space-between'}}>
-                      <Text style={styles.title}>{item.name}</Text>
-                      <Text style={styles.text}>{item.total_label} {item.total}</Text>
-                    </View>
-                    { open ? (
-                    <FlatList 
-                      data={item.fields}
-                      renderItem={({item}) => (
-                        <View style={{flexDirection:'row', justifyContent:'space-between', margin:3}}>
-                          <Text style={styles.title} >{item.label} </Text>
-                          <Text style={styles.text} >{item.value} </Text>
-                        </View>
-                      )}
-                    />
-                    ):(
-                      null
-                    )}
-                  </View>
-                </View>
-              </View>
-            )}
+            renderItem={({ item }) => <Row item={item}/> }
             keyExtractor={(item, index) => index}
           />
       </View>      
